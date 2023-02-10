ac224f5649622def7a9b53ea0029bd7884b12fa2
Tam55si <117524132+Tam55si@users.noreply.github.com>
2023-02-10 23:34:58 +0530

    first commit

---
+++ hackathon-pop-quiz-build-1.0.1/pop-quiz-1.0.1/src/js/index.js
@@ -0,0 +1,31 @@
+// The following code starts Mock Service Worker tool which allows you to simulate a backend (an API) for building your apps that talk to a remote service. You can visit https://mswjs.io for details on this utility and check src/api/routes.js for a sample API route that you can edit/create as needed to simulate a real world API. This simulated backend will not be part of the completed application (built edition) and you must use a real world backend built using Node.js + Express or Java + Spring Boot to provide such a service.
+
+// If you do not require a simulated backend, you can remove the code shown below.
+if (process.env.NODE_ENV === 'development') {
+  const apiStatus = document.querySelector('#api-status');
+  import('../api/browser')
+    .then(({ worker }) => worker.start())
+    .then(() => fetch('/'))
+    .then((res) => res.json())
+    .then((res) => (apiStatus.innerText = res.message));
+}
+
+let modalBtns = [...document.querySelectorAll(".button")];
+      modalBtns.forEach(function (btn) {
+        btn.onclick = function () {
+          let modal = btn.getAttribute("data-modal");
+          document.getElementById(modal).style.display = "block";
+        };
+      });
+      let closeBtns = [...document.querySelectorAll(".close")];
+      closeBtns.forEach(function (btn) {
+        btn.onclick = function () {
+          let modal = btn.closest(".modal");
+          modal.style.display = "none";
+        };
+      });
+      window.onclick = function (event) {
+        if (event.target.className === "modal") {
+          event.target.style.display = "none";
+        }
+      };
